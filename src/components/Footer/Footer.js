f2633c4030f056d61db875308af8fa938280d38b
Shubham Sharma <shubhamsharma@Shubhams-MacBook-Air.local>
2021-08-27 22:51:17 +0530

    footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <hr style={{ width: "90%", marginTop: 20 }} />
-      <span className="name">
-        Made by{" "}
-        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
-          Shubham Sharma
-        </a>
-      </span>
+      <span className="name">Made by Shubham Sharma</span>
     </div>
   );
 };
